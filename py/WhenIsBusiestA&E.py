dadaba4d0d479040d00b4672b5ac047e74c04a72
DC Thomson Data Team <datateam@dctmedia.co.uk>
2025-02-04 14:24:12 +0000

    Update WhenIsBusiestA&E.py
    Changed source to most up to date link. Also made changes to the department type filtering column. Source changed from emergency department/minor injury unit to type 1 or type 3. Source linked in code says emergency is type 1, so changed that to filter on type 1 instead.

--- py/WhenIsBusiestA&E.py
+++ py/WhenIsBusiestA&E.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #sources
-When = pd.read_csv("https://www.opendata.nhs.scot/dataset/997acaa5-afe0-49d9-b333-dcf84584603d/resource/022c3b27-6a58-48dc-8038-8f1f93bb0e78/download/opendata_monthly_ae_when_202305.csv")
+When = pd.read_csv("https://www.opendata.nhs.scot/dataset/997acaa5-afe0-49d9-b333-dcf84584603d/resource/022c3b27-6a58-48dc-8038-8f1f93bb0e78/download/opendata_monthly_ae_when_202412.csv")
 healthboards = pd.read_csv("https://www.opendata.nhs.scot/dataset/9f942fdb-e59e-44f5-b534-d6e17229cc7b/resource/652ff726-e676-4a20-abda-435b98dd7bdc/download/hb14_hb19.csv")
 
 #merging sources
@@ -13,7 +13,8 @@ Whenmerge['Day'] = pd.Categorical(Whenmerge['Day'], categories=cats, ordered=Tru
 df_weekday = Whenmerge.sort_values('Day')
 
 #filtering to get rid of minor injury units
-df_weekday2= df_weekday[df_weekday['DepartmentType']=='Emergency Department']
+#re-coding edit note, data here changed from Emergency Department/MIU to Type 1/3. Type 1 is emergency department, source for this here: https://publichealthscotland.scot/resources-and-tools/health-intelligence-and-data-management/national-data-catalogue/data-dictionary/search-the-data-dictionary/accident-and-emergency-ae-service/
+df_weekday2= df_weekday[df_weekday['DepartmentType']=='Type 1']
 
 #renaming columns
 df_weekday3= df_weekday2.rename(columns={'NumberOfAttendances': 'Number of attendances'})
